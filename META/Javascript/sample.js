ab9561785ddbce1bb8ff0146c1a31e411aa54832
arunshajizn333 <154526818+arunshajizn333@users.noreply.github.com>
2026-07-30 15:47:37 +0530

    javascripts conditional statemets practice

---
+++ META/Javascript/sample.js
@@ -0,0 +1,24 @@
+var Day = "Sunday";
+
+switch(Day){
+    case "Monday":
+        console.log('Do something');
+        break;
+    case "Tuesday":
+        console.log('Do something else');
+        break;      
+    case "Wednesday":
+        console.log('Do another thing');
+        break;
+    case "Thursday": 
+        console.log('Do something different');  
+        break;
+    case "Sunday":
+        console.log('Do nothing');
+        break;
+    default:
+        console.log('Invalid day');
+        break;
+
+    
+}
