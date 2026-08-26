7f78cbd4e82016977d2b3f0ec3e64d29b3a2cc16
arunshajizn333 <154526818+arunshajizn333@users.noreply.github.com>
2026-08-26 12:49:06 +0530

    HOT -more optimized with array.prototype and map

--- Namaste JS/higher-order-fn.js
+++ Namaste JS/higher-order-fn.js
@@ -12,6 +12,8 @@ const diameter = function (radius) {
     return 2 * radius;
 }
 
+// Normal Way of writing HOF
+
 const calculate = function (radius, logic) {
     let output = [];
     for (let i = 0; i < radius.length; i++) {
@@ -20,6 +22,26 @@ const calculate = function (radius, logic) {
     return output;
 }
 
-console.log("area : ",calculate(radius, area))
-console.log("circumferance : ",calculate(radius, circumferance))
-console.log("diameter : ",calculate(radius, diameter))
+// console.log("area : ",calculate(radius, area))
+// console.log("circumferance : ",calculate(radius, circumferance))
+// console.log("diameter : ",calculate(radius, diameter))
+
+
+// Making HOT more Optimised
+
+Array.prototype.calculate = function (logic) {
+    let output = [];
+    for (let i = 0; i < this.length; i++) {
+        output.push(logic(this[i]));
+    }
+    return output;
+}
+// console.log("area : ",radius.calculate(area))
+// console.log("circumferance : ",radius.calculate(circumferance))
+// console.log("diameter : ",radius.calculate(diameter))
+
+// By using Map
+
+console.log("area : ",radius.map(area))
+console.log("circumferance : ",radius.map(circumferance))
+console.log("diameter : ",radius.map(diameter))
