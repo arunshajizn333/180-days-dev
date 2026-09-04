04745504954234b83395673a7ef74dfeaa949893
arunshajizn333 <154526818+arunshajizn333@users.noreply.github.com>
2026-09-04 18:03:25 +0530

    exercises -Level 2

--- Namaste JS/Exercies/Level2.js
+++ Namaste JS/Exercies/Level2.js
@@ -7,5 +7,40 @@ async function main() {
     console.log(data);
 }
 
-main()
+// main()
 
+// Q7. Sequential execution
+
+const p1= new Promise ((res,rej)=>{
+    setTimeout(function(){
+        res("First")
+    },2000)
+
+})
+const p2= new Promise ((res,rej)=>{
+    setTimeout(function(){
+        res("Second")
+    },3000)
+
+})
+const p3= new Promise ((res,rej)=>{
+    setTimeout(function(){
+        res("Third")
+    },4000)
+
+})
+
+// async function sequential() {
+//     const data1=await p1
+//     console.log(data1);
+    
+//     const data2=await p2
+//      console.log(data2);
+    
+//     const data3=await p3
+//      console.log(data3);
+    
+// }
+// sequential()
+const data=Promise.all([p1,p2,p3])
+console.log(await data);
