bad6291aef4a3dd06b34d44f3dc01375634a613a
arunshajizn333 <154526818+arunshajizn333@users.noreply.github.com>
2026-09-14 22:29:15 +0530

    Introduction to JSX

--- REACT JS/Namaste React/App.js
+++ REACT JS/Namaste React/App.js
@@ -1,6 +1,11 @@
-const root=ReactDOM.createRoot(document.getElementById("root"))
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-{/* <div id="parent">
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+{
+  /* <div id="parent">
     <div id="child1">
         <h1>Am Child 1 H1 tag</h1>
         <h2>Am Child 1 H2 tag</h2>
@@ -9,26 +14,27 @@ const root=ReactDOM.createRoot(document.getElementById("root"))
         <h1>Am Child 2 H1 tag</h1>
         <h2>Am Child 2 H2 tag</h2>
     </div>
-</div> */}
+</div> */
+}
+
+// const Parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "chid1" }, [
+//     React.createElement("h1", {}, "Am Child 1 H1 tag"),
+//     React.createElement("h2", {}, "Am Child 1 H2 tag"),
+//   ]),
+//   React.createElement("div", { id: "chid2" }, [
+//     React.createElement("h1", {}, "Am Child 2 H1 tag"),
+//     React.createElement("h2", {}, "Am Child 2 H2 tag"),
+//   ]),
+// ]);
+
+// JSX
+
+const heading = (
+  <h1 className="head" tabIndex={5}>
+    namaste javascript
+  </h1>
+);
+root.render(heading);
 
 
-const Parent=React.createElement("div",{id : "parent"},[
-    React.createElement("div",{id : "chid1"},[
-         React.createElement("h1",{},"Am Child 1 H1 tag"),
-         React.createElement("h2",{},"Am Child 1 H2 tag")
-    ]),
-     React.createElement("div",{id : "chid2"},[
-         React.createElement("h1",{},"Am Child 2 H1 tag"),
-         React.createElement("h2",{},"Am Child 2 H2 tag")
-    ])
-])
-    
-
-
-
-
-
-
-
-
-root.render(Parent)
